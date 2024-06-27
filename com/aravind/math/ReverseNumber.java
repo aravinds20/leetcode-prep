a7799c406107e7e192f11b16dadb610085263cd1
aravind <aravinds.cb@gmail.com>
2024-06-27 21:27:52 +0530

    Exercise to reverse a number.

---
+++ com/aravind/math/ReverseNumber.java
@@ -0,0 +1,20 @@
+package com.aravind.math;
+
+
+/*
+Problem statement - reverse a number.
+ */
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        int num = 123456;
+        int reverseNumber = 0;
+        while(num != 0) {
+            reverseNumber = (num % 10) *10;
+            num = num/10;
+            reverseNumber = reverseNumber + num /10;
+        }
+        System.out.println(reverseNumber);
+    }
+
+}
